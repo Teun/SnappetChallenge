ba67d2f580a5e0b5380b6753e14930f77cd507c2
Robert Vukovic <rvukovic@gmail.com>
2015-09-02 21:49:13 +0200

    Code cleanup

--- Snappet/Snappet/Models/DashboardViewModel.cs
+++ Snappet/Snappet/Models/DashboardViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using DotNet.Highcharts;
+﻿using DotNet.Highcharts;
 using Snappet.Data;
 
 namespace Snappet.Models
