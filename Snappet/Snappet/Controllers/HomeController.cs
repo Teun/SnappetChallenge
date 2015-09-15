3be9b80a96ebaae7d2b05fab2e57b08b2d4bc345
Joosten81 <sjefjoosten@outlook.com>
2015-09-15 11:18:38 +0200

    Added code for Snappet Challenge

---
+++ Snappet/Snappet/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Snappet.Models.ViewModels;
+using Snappet.Models.DataModels;
+
+namespace Snappet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<AverageProgress> averages = DataSource.AverageProgress();
+
+            ViewBag.Subjects = JsonConvert.SerializeObject(averages.Select(a => a.Subject.ToString()));
+            ViewBag.Averages = JsonConvert.SerializeObject(averages.Select(a => a.Average));
+
+            var model = new GraphModel();
+            var todaysAnswers = DataSource.TodayResults();
+            model.UserIds = todaysAnswers.Select(u => u.UserId).Distinct();
+
+            if(Request.IsAjaxRequest())
+            {
+                var userId = Convert.ToInt32(Request.Form["DdlUserId"].ToString());
+                var userAverage = todaysAnswers.Where(u => u.UserId == userId).GroupBy(g => g.Subject, p => p.Progress).Select(g => new AverageProgress
+                {
+                    Subject = g.Key,
+                    Average = g.Average()
+                });
+
+                model.SubjectJson = JsonConvert.SerializeObject(userAverage.Select(ua => ua.Subject.ToString()));
+                model.AveragesJson = JsonConvert.SerializeObject(userAverage.Select(ua => ua.Average));
+
+                return PartialView("_UserProgress", model);
+            }
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Snappet/Snappet/Models/DataModels/SubmittedAnswer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Snappet.Models.DataModels
+{
+    public class SubmittedAnswer
+    {
+        public int SubmittedAnswerId { get; set; }
+
+        public DateTime SubmitDateTime { get; set; }
+
+        public int Correct { get; set; }
+        
+        public int Progress { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ExcerciseId { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Domain { get; set; }
+
+        public string LearningObjective { get; set; }
+    }
+}
