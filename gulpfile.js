2e62d3d6e87fcc09fdcb072b37ff81c43716c408
pm5544 <joel@pm5544.eu>
2016-02-15 19:57:18 +0100

    added the copying of the knockout outut file to the dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
+var rename = require("gulp-rename");
 
 gulp.task('clean', function() {
   // You can use multiple globbing patterns as you would with `gulp.src`
@@ -11,5 +12,11 @@ gulp.task('parse', ['clean'], function(){
     require('./node/parseData');
 });
 
+gulp.task('knockout', function(){
+    return gulp.src('node_modules/knockout/build/output/knockout-latest.js')
+        .pipe(rename('ko.js'))
+        .pipe(gulp.dest('dist'))
+    ;
+});
 
-gulp.task('default', ['parse']);
+gulp.task('default', ['parse', 'knockout']);
