c400a5cff54d5d004310f2fddeb875ec430aa4a5
pm5544 <joel@pm5544.eu>
2016-02-16 10:48:13 +0100

    made the clean and parse tasks run in sequence to counter some race conditions while building

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,17 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var mqpacker = require('css-mqpacker');
 var csswring = require('csswring');
+var runSequence = require('run-sequence');
 
 gulp.task('clean', function() {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  return del(['dist','parsed']);
+    return del(['dist','parsed']);
 });
 
-// run the parse first to get all the data mangled
-gulp.task('parse', ['clean'], function(){
+gulp.task('parse', function(){
     require('./node/parseData');
 });
 
-gulp.task('knockout',['clean'], function(){
+gulp.task('knockout', function(){
     return gulp.src('node_modules/knockout/build/output/knockout-latest.js')
         .pipe(rename('ko.js'))
         .pipe(gulp.dest('dist'))
@@ -50,8 +49,9 @@ gulp.task('watch', function(){
     gulp.watch('src/js/*,js', ['js']);
     gulp.watch('src/html/*.html', ['html']);
     gulp.watch('src/css/*.css', ['css']);
-    // gulp.watch(img, ['img']);
 });
 
 
-gulp.task('default', ['parse', 'knockout', 'html', 'css', 'js', 'watch']);
+gulp.task('default', function(){
+    runSequence('clean', 'parse', ['knockout', 'html', 'css', 'js', 'watch']);
+});
