e2af0a651fbef44f1fe09aa0ed7ae4d66a448731
Prasing,Niels N.H.H <n.prasing@student.fontys.nl>
2016-05-23 19:50:41 +0200

    basic application start up

---
+++ Snappet Implementation/Snappet/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Snappet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ Snappet Implementation/Snappet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Snappet.Startup))]
+namespace Snappet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
