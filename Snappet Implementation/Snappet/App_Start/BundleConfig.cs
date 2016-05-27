aaf4e21543bb8cb4af20a5187f30d160e6e0594b
Prasing,Niels N.H.H <n.prasing@student.fontys.nl>
2016-05-27 14:01:51 +0200

    unused javascripts files deleted and using .min.js files now

--- Snappet Implementation/Snappet/App_Start/BundleConfig.cs
+++ Snappet Implementation/Snappet/App_Start/BundleConfig.cs
@@ -8,10 +8,7 @@ namespace Snappet
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery-1.10.2.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -19,8 +16,8 @@ namespace Snappet
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
                       "~/Scripts/index.js",
