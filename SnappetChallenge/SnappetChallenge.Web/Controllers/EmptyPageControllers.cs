2d2313407732e4efda9f810a319c99c3602bb6df
Nico Beemster <nico.beemster@gmail.com>
2017-05-05 17:45:24 +0200

    No idea what these changes are but lets keep em..

--- SnappetChallenge/SnappetChallenge.Web/Controllers/EmptyPageControllers.cs
+++ SnappetChallenge/SnappetChallenge.Web/Controllers/EmptyPageControllers.cs
@@ -1,4 +1,7 @@
-﻿namespace SnappetChallenge.Web.Controllers
+﻿using System.Collections.Generic;
+using SnappetChallenge.Web.Models;
+
+namespace SnappetChallenge.Web.Controllers
 {
     using System.Web.Mvc;
 
@@ -17,6 +20,22 @@
         public ActionResult Progress()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            var model = new ListModel
+            {
+                ServerNames = new List<Server>
+                {
+                    new Server { Id = 1, Name = "Server1"},
+                    new Server { Id = 2, Name = "Server2"},
+                    new Server { Id = 3, Name = "Server3"},
+                }
+            };
+
+            return View(model);
+
         }
     }
 }
