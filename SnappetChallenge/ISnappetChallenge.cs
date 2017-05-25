fc90a006cc9775442e9b2fc2c420be95ec8c9101
MarqueICT <mark@marqueict.nl>
2017-05-25 20:27:58 +0200

    Add files via upload

---
+++ SnappetChallenge/ISnappetChallenge.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SnappetChallenge
+{
+    [ServiceContract]
+    public interface ISnappetChallenge
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "GetProgressOverViewDate/{Date}/{Time}", ResponseFormat = WebMessageFormat.Json)]
+        List<ProgressOverView> GetProgressOverViewDate(string Date, string Time);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetDomainAnswersForDateTime/{UserID}/{Domain}/{Date}/{Time}", ResponseFormat = WebMessageFormat.Json)]
+        List<Answer> GetDomainAnswersForDateTime(string UserID, string Domain, string Date, string Time);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetAnswer/{answerId}", ResponseFormat = WebMessageFormat.Json)]
+        Answer GetAnswer(string answerId);
+    }
+
+
+    [DataContract]
+    public class Answer
+    {
+        [DataMember]
+        public int SubmittedAnswerId { get; set; }
+
+        [DataMember]
+        public DateTime SubmitDateTime { get; set; }
+
+        [DataMember]
+        public string Correct { get; set; }
+
+        [DataMember]
+        public string Progress { get; set; }
+
+        [DataMember]
+        public string UserId { get; set; }
+
+        [DataMember]
+        public string ExerciseId { get; set; }
+
+        [DataMember]
+        public string Difficulty { get; set; }
+
+        [DataMember]
+        public string Subject { get; set; }
+
+        [DataMember]
+        public string Domain { get; set; }
+
+        [DataMember]
+        public string LearningObjective { get; set; }
+    }
+
+    public class ProgressOverView
+    {
+        [DataMember]
+        public string UserId { get; set; }
+
+        [DataMember]
+        public string Domain { get; set; }
+
+        [DataMember]
+        public int TotalProgress { get; set; }
+
+    }
+}
---
+++ SnappetChallenge/SQL/tWork.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE tWork
+(
+	SubmittedAnswerId	INT NOT NULL PRIMARY KEY,
+	SubmitDateTime		DATETIME NOT NULL,
+	Correct				BIT NOT NULL,
+	Progress			INT NOT NULL,
+	UserId				INT NOT NULL,
+	ExerciseId			INT NOT NULL,
+	Difficulty			INT NOT NULL,
+	Subject				VARCHAR(255) NULL,
+	Domain				VARCHAR(255) NULL,
+	LearningObjective	VARCHAR(255) NULL
+)
+GO
