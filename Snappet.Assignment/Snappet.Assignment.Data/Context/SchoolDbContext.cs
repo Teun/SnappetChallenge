21600fde82531cceca84a7b681d3cd12c103e6ee
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-06 22:24:39 +0300

    Create SchoolDbContext and adding DbSet for User,Exercise and Work

---
+++ Snappet.Assignment/Snappet.Assignment.Data/Context/SchoolDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Snappet.Assignment.Entities.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snappet.Assignment.Data.Context
+{
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Work> Works { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+        }
+    }
+}
