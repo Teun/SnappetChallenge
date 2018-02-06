e21054adc32c3fdc9d145f124af73b160d853e8a
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-06 23:59:42 +0300

    Create Test categories

---
+++ Snappet.Assignment/Snappet.Assignment.Tests/TestCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snappet.Assignment.Tests
+{
+    public static class TestCategory
+    {
+        public const string UnitTest = "Unit Tests";
+        public const string WorkRepository = "Work Repository Tests";
+        public const string PositiveCases = "Positive Cases";
+        public const string NegativeCases = "Negative Cases";
+      
+    }
+}
