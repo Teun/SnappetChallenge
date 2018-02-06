f9b2e0aa207c0dd128b99cb995ec4350144e6d79
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-07 00:27:00 +0300

    create Integration tests for WorkRepository

---
+++ Snappet.Assignment/Snappet.Assignment.IntergrationTests/Repositories/WorRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Snappet.Assignment.Business.Interfaces;
+using Snappet.Assignment.Business.UnitOfWork;
+using Snappet.Assignment.Entities.DomainObjects;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace Snappet.Assignment.IntergrationTests.Repositories
+{
+    [TestFixture]
+    [Category(TestCategory.WorkRepository)]
+    [Category(TestCategory.IntegrationTests)]
+    public class WorRepositoryTests
+    {
+        private IUnitOfWork _uow;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uow = new UnitOfWork(GlobalSetUp.ConnectionString);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _uow = null;
+        }
+
+        [Test]
+        public async Task Get_AllWorks_ShouldReturnListOfWork()
+        {
+
+            //Arrrange
+            var expected = GlobalSetUp.Works;
+
+            //Act
+            var result = (await _uow.WorkRepository.GetAllAsync()).ToList();
+
+
+            //Assert
+            result.Should().HaveCount(c => c == expected.Count());
+
+
+        }
+
+        //Tuesday 2015-03-24 11:30:00 
+        [TestCase(2015, 03, 24, 12, 00, 00)]
+        [TestCase(2015, 03, 24, 11, 30, 00)]
+        [TestCase(2015, 03, 24, 10, 30, 00)]
+        [TestCase(2015, 03, 24, 10, 00, 00)]
+
+        public async Task Get_TodayWorks_ShoulReturnOnlyTodayWork(short year, byte month, byte day, byte hour, byte minute, byte second)
+
+        {
+            //Arrange
+            var today = new DateTime(year, month, day, hour, minute, second);
+
+            Func<Work, bool> predicate = c => c.SubmitDateTime.Day == today.Day && c.SubmitDateTime.Hour <= today.Hour && c.SubmitDateTime.Minute <= today.Minute;
+            Expression<Func<Work, bool>> expressionPredicate = c => c.SubmitDateTime.Day == today.Day && c.SubmitDateTime.Hour <= today.Hour && c.SubmitDateTime.Minute <= today.Minute;
+
+            var expected = GlobalSetUp.Works.Where(predicate).ToList();
+
+            //Act
+            var result = (await _uow.WorkRepository.GetAllAsync(expressionPredicate)).ToList();
+
+            //Assert
+
+            result.Should().HaveCount(c => c == expected.Count());
+        }
+
+    }
+}
---
+++ Snappet.Assignment/Snappet.Assignment.IntergrationTests/TestCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Snappet.Assignment.IntergrationTests
+{
+    public static class TestCategory
+    {
+        public const string IntegrationTests = "Integration Tests";
+        public const string WorkRepository = "Work Repository Tests";
+        public const string WorkController = "Work Controller Tests";
+
+
+    }
+}
