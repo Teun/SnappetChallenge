f04cb933a3a248aaead64b864d9336db73b6c51a
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-07 00:34:12 +0300

    Adding Data Transfer Objects

---
+++ Snappet.Assignment/Snappet.Assignment.Entities/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Snappet.Assignment.Entities.DTOs
+{
+    public class UserDto
+    {       
+
+        public string Name { get; set; }
+    }
+}
---
+++ Snappet.Assignment/Snappet.Assignment.Entities/DTOs/WorkDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Snappet.Assignment.Entities.DTOs
+{
+    public class WorkDto
+    {
+       
+
+        public DateTime SubmitDateTime { get; set; }
+
+        public bool Correct { get; set; }
+
+        public short Progress { get; set; }
+
+
+        public float? Difficulty { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Domain { get; set; }
+
+        public string LearningObjective { get; set; }
+
+
+        public UserDto User { get; set; }
+
+        public ExerciseDto Exercise { get; set; }
+    }
+}
