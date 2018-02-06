592cb2e51c087ebff910feaddc2a83bf4d19844a
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-07 00:57:18 +0300

    Create Work web api and remove unnecessary views,models and files form the application

--- Snappet.Assignment/Snappet.Assignment.WebApp/Controllers/HomeController.cs
+++ Snappet.Assignment/Snappet.Assignment.WebApp/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Snappet.Assignment.WebApp.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Snappet.Assignment.WebApp.Controllers
 {
@@ -15,23 +9,6 @@ namespace Snappet.Assignment.WebApp.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
--- Snappet.Assignment/Snappet.Assignment.WebApp/Startup.cs
+++ Snappet.Assignment/Snappet.Assignment.WebApp/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Snappet.Assignment.Data.Context;
 using Microsoft.EntityFrameworkCore;
+using Snappet.Assignment.Entities.Mapping;
+using Snappet.Assignment.Entities.Interfaces;
+using Snappet.Assignment.Business.Interfaces;
+using Snappet.Assignment.Business.UnitOfWork;
 
 namespace Snappet.Assignment.WebApp
 {
@@ -28,9 +32,13 @@ namespace Snappet.Assignment.WebApp
             services.AddDbContext<SchoolDbContext>(options =>
                 options.UseSqlServer(connectionString, x => x.MigrationsAssembly("Snappet.Assignment.WebApp")));
 
-            services.AddMvc();
-        }
+            services.AddMvc()
+                    .AddJsonOptions(
+                    options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
+            services.AddSingleton<IMapper, Mapper>();
+            services.AddScoped<IUnitOfWork>(c => new UnitOfWork(connectionString));
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -46,11 +54,18 @@ namespace Snappet.Assignment.WebApp
 
             app.UseStaticFiles();
 
+
+
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                  name: "spa-fallback",
+                  defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
