e13de1acc6a87a94aa23a968568729c1fd28ddf0
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-06 22:15:33 +0300

    Create Domain objects (User, Exercise and Work )

---
+++ Snappet.Assignment/Snappet.Assignment.Entities/DomainObjects/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snappet.Assignment.Entities.DomainObjects
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+       
+    }
+}
---
+++ Snappet.Assignment/Snappet.Assignment.Entities/DomainObjects/Work.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snappet.Assignment.Entities.DomainObjects
+{
+    public class Work
+    {
+        public int SubmittedAnswerId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ExerciseId { get; set; }
+
+        public DateTime SubmitDateTime { get; set; }
+
+        public bool Correct { get; set; }
+
+        public short Progress { get; set; }
+
+
+        public double? Difficulty { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Domain { get; set; }
+
+        public string LearningObjective { get; set; }
+
+
+       
+
+    }
+}
