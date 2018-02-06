d0488c0f2f00fe066eb69ff0560d0ea04229f06d
ahmedeljabry <ahmed.eljabry@gmail.com>
2018-02-07 01:14:44 +0300

    Create UI and adding AngularJS services, controllers and routing

---
+++ Snappet.Assignment/Snappet.Assignment.WebApp/wwwroot/js/routing/routing.js
@@ -0,0 +1,21 @@
+﻿app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.caseInsensitiveMatch = true;
+    $routeProvider
+        .when("/", {
+            templateUrl: "html/work/all-works-tmp.html", 
+            controller: "WorkCtrl"
+        })
+        .when("/home", {
+            templateUrl: "html/work/all-works-tmp.html", 
+            controller: "WorkCtrl"
+        })       
+       
+       
+        .when("/today-works", {
+            templateUrl: "html/work/today-works-tmp.html",
+            controller: "WorkCtrl"
+        })
+        .otherwise({ redirectTo: '/home' });
+  
+    $locationProvider.html5Mode(true);
+})
