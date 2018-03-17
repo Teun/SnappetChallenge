96f3b773bac89920ec6d84a3de32f3ad8e3db167
hello-nn <noornayeem@gmail.com>
2018-03-16 18:03:09 -0600

    Task Completed

---
+++ SnappetSolution/Snappet/Services/IWorkResultService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Snappet.Models;
+
+namespace Snappet.Services
+{
+    public interface IWorkResultService
+    {
+        IList<WorkResult> GetResults();
+        IList<WorkResult> GetResults(DateTime startDate, DateTime endDate);
+    }
+}
---
+++ SnappetSolution/Snappet/Services/WorkResultService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using Snappet.Models;
+
+namespace Snappet.Services
+{
+    public class WorkResultService: IWorkResultService
+    {
+        private List<WorkResult> workResults = new List<WorkResult>();
+
+        public WorkResultService()
+        {            
+            string path = System.Web.HttpContext.Current.Server.MapPath(WebConfigurationManager.AppSettings["JsonFilePath"]);
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                workResults = JsonConvert.DeserializeObject<List<WorkResult>>(json);
+            }
+        }
+
+        public IList<WorkResult> GetResults()
+        {
+            return workResults;
+        }
+
+        public IList<WorkResult> GetResults(DateTime startDate, DateTime endDate)
+        {
+            return workResults.Where(w => w.SubmitDateTime >= startDate && w.SubmitDateTime < endDate).ToList();
+        }
+    }
+}
