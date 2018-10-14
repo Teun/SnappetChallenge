49c388709a0c6fc79f5cc01fc6c9e4e2922f9041
christiaan <christiaan_n64@hotmail.com>
2018-10-14 16:49:21 +0200

    Snappet Challenge

---
+++ FrontEnd/ClientApp/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatFormFieldModule, MatNativeDateModule, MatDatepickerModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    FetchDataComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'results', component: FetchDataComponent },
+    ]),
+    BrowserAnimationsModule,
+    MatInputModule, MatFormFieldModule, MatNativeDateModule, MatDatepickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WebApi/Models/WorkItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WebApi.Models
+{
+    public class WorkItem
+    {
+        public int SubmittedAnswerId { get; set; }
+
+        public DateTime SubmitDateTime { get; set; }
+
+        public bool Correct { get; set; }
+
+        public int Progress { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ExerciseId { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Domain { get; set; }
+
+        public string LearningObjective { get; set; }
+    }
+}
