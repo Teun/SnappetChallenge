e394fd12fa994b456526815aa2e6a1d35d9c5df6
Octavio Fernandes <octaviobffernandes@gmail.com>
2019-02-22 07:42:34 +0100

    Add missing dto file to source control

---
+++ Src/DTOs/GetProgressDTO.cs
@@ -0,0 +1,17 @@
+﻿using StudentsAPI.WebApi.Definitions;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsAPI.WebApi.DTOs
+{
+    public class GetProgressDTO
+    {
+        public Domain? Domain { get; set; }
+
+        [Required]
+        public int? Month { get; set; }
+
+        [Required]
+        public int? Year { get; set; }
+    }
+}
