eb708e13f7be373d1240e6db3b45bc58ea319e82
SnappetChallenger <anonymous@local>
2019-02-26 22:36:09 +0100

    Create basic data model and repository interface

---
+++ SnappetTrueskill/SnappetTrueskill/ExerciseInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SnappetTrueskill
+{
+    public class ExerciseInteraction
+    {
+        public int SubmittedAnswerId { get; }
+        public DateTime SubmitDateTime { get; }
+        public bool Correct { get; }
+        public int Progress { get; }
+        public int UserId { get; }
+        public int ExerciseId { get; }
+        public double Difficulty { get; }
+        public string Subject { get; }
+        public string Domain { get; }
+        public string LearningObjective { get; }
+    }
+}
---
+++ SnappetTrueskill/SnappetTrueskill/IExerciseInteractionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnappetTrueskill
+{
+    public interface IExerciseInteractionRepository : IDisposable
+    {
+        IEnumerable<ExerciseInteraction> GetAll();
+        ExerciseInteraction Get(int id);
+        void Insert(ExerciseInteraction interaction);
+        void Delete(int id);
+        void Update(ExerciseInteraction interaction);
+        void Save();
+    }
+}
