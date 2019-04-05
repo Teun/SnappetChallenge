7ea737d127a0ba37aae030f32957a07b76c65fa5
Polhovsky <paulroggeveen@hotmail.com>
2019-04-05 23:20:04 +0200

    update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import dash_html_components as html
 
 import plotly.graph_objs as go
 
-import pytz
-from dateutil import parser
-import datetime
 import itertools
 
 tabs_style = {
@@ -68,46 +65,8 @@ beschrijving_list_item = {
     'marginBottom': 0
 }
 
-local_tz = pytz.timezone('Europe/Amsterdam')
-
-def utc_to_local(utc_dt):
-    local_dt = utc_dt.replace(tzinfo = pytz.utc).astimezone(local_tz)
-    return local_tz.normalize(local_dt)
-
-def aslocaltimestr(utc_dt):
-    return utc_to_local(utc_dt).strftime('%Y-%m-%d %H:%M:%S')
-
 # import data
-snappet_01 = pd.read_csv('data.txt', sep = '\t', encoding = 'ISO-8859-1', low_memory = False)
-
-# convert datetime UTC to local
-snappet_01['Datetime_UTC'] = snappet_01['SubmitDateTime'].apply(lambda x: parser.parse(x))
-snappet_01['Datetime_local'] = snappet_01['Datetime_UTC'].apply(lambda x: pd.to_datetime(aslocaltimestr(x)))
-
-# create extra columns
-snappet_01['Date'] = snappet_01['Datetime_local'].apply(lambda x: x.date())
-snappet_01['Time'] = snappet_01['Datetime_local'].apply(lambda x: x.time())
-snappet_01['Hour'] = snappet_01['Datetime_local'].apply(lambda x: x.hour)
-snappet_01['Hour_string'] = snappet_01['Hour'].apply(lambda x: str(x).zfill(2) + ' - ' + str(x + 1).zfill(2) + ' uur')
-snappet_01['Group'] = np.where(snappet_01['Subject'] == 'Rekenen', snappet_01['Domain'], snappet_01['Subject'])
-
-def color_assignment(row):
-    if row['Group'] == 'Begrijpend Lezen':
-        return 'rgba(255, 255, 0, 0.8)'
-    elif row['Group'] =='Getallen':
-        return 'rgba(0, 128, 0, 0.8)'
-    elif row['Group'] =='Meten':
-        return 'rgba(255, 165, 0, 0.8)' 
-    elif row['Group'] =='Spelling':
-        return 'rgba(0, 128, 128, 0.8)'
-    elif row['Group'] =='Verbanden':
-        return 'rgba(255, 20, 147, 0.8)'
-    elif row['Group'] =='Verhoudingen':
-        return 'rgba(210, 105, 30, 0.8)'
-    else:
-        return 'foutje'
-
-snappet_01['Color'] = snappet_01.apply(color_assignment, axis = 1)
+snappet_01 = pd.read_csv('data_bewerkt.txt', sep = '\t', encoding = 'ISO-8859-1', low_memory = False)
 
 # create lists with options for dropdown-menus
 dag_options = [{'label': option, 'value': option} for option in list(snappet_01['Date'].unique())]
@@ -323,7 +282,7 @@ def update_graph_activiteit(dag_activiteit):
     
     # samenvoegen
     
-    snappet_09 = pd.concat([snappet_05, snappet_08], axis = 0, sort = False)
+    snappet_09 = pd.concat([snappet_05, snappet_08], axis = 0)
 
     all_groups = list(snappet_01['Group'].unique())
     all_leerlingen = list(snappet_09['UserId'].unique())
