1130c06b36c211180fa7dc93c7bb4e924f0e34da
Polhovsky <paulroggeveen@hotmail.com>
2019-04-06 21:39:15 +0200

    update data_bewerken.py

--- data_bewerken.py
+++ data_bewerken.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 
 import pytz
-from dateutil import parser
-import datetime
 
 local_tz = pytz.timezone('Europe/Amsterdam')
 
@@ -18,12 +16,11 @@ def aslocaltimestr(utc_dt):
 snappet_01 = pd.read_csv('data.txt', sep = '\t', encoding = 'ISO-8859-1', low_memory = False)
 
 # convert datetime UTC to local
-snappet_01['Datetime_UTC'] = snappet_01['SubmitDateTime'].apply(lambda x: parser.parse(x))
+snappet_01['Datetime_UTC'] = pd.to_datetime(snappet_01['SubmitDateTime'])
 snappet_01['Datetime_local'] = snappet_01['Datetime_UTC'].apply(lambda x: pd.to_datetime(aslocaltimestr(x)))
 
 # create extra columns
 snappet_01['Date'] = snappet_01['Datetime_local'].apply(lambda x: x.date())
-snappet_01['Time'] = snappet_01['Datetime_local'].apply(lambda x: x.time())
 snappet_01['Hour'] = snappet_01['Datetime_local'].apply(lambda x: x.hour)
 snappet_01['Hour_string'] = snappet_01['Hour'].apply(lambda x: str(x).zfill(2) + ' - ' + str(x + 1).zfill(2) + ' uur')
 snappet_01['Group'] = np.where(snappet_01['Subject'] == 'Rekenen', snappet_01['Domain'], snappet_01['Subject'])
