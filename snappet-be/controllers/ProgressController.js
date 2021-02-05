0a2fe09707e26a089dca971127af706e55a98a12
Chris Kirk <uberengineer@gmail.com>
2021-02-05 14:11:27 +0100

    repushing for Fe

--- snappet-be/controllers/ProgressController.js
+++ snappet-be/controllers/ProgressController.js
@@ -3,21 +3,21 @@ const { setHours, setMinutes, endOfDay, startOfDay } = require("date-fns");
 const Progress = require("../models/Progress");
 
 //List all available progresss from database....
-exports.listAllProgress = async (req, res) => {
+// exports.listAllProgress = async (req, res) => {
 
-  Progress.find({
-    UserId: 40281
-    // SubmitDateTime: {
-    //   $lte: Date.UTC(2015, 03, 15, 11, 30, 00, 00),
-    //   $gte: Date.UTC(2015, 03, 15, 00, 00, 00, 00),
-    // }
-  }, (err, progress) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).json(progress);
-  });
-};
+//   Progress.find({
+//     UserId: 40281
+//     // SubmitDateTime: {
+//     //   $lte: Date.UTC(2015, 03, 15, 11, 30, 00, 00),
+//     //   $gte: Date.UTC(2015, 03, 15, 00, 00, 00, 00),
+//     // }
+//   }, (err, progress) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//     res.status(200).json(progress);
+//   });
+// };
 
 // read a perticular progress by _id......
 exports.getProgressByDate = (req, res) => {
