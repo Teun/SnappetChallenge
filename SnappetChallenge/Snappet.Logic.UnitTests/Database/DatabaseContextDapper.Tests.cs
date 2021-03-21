c8ebc3f24fe1b5c2bd1ed243c3675f67f64d3c3e
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-21 15:49:20 +0330

    Create DatabaseContextDapper.Tests.cs

---
+++ SnappetChallenge/Snappet.Logic.UnitTests/Database/DatabaseContextDapper.Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Snappet.Logic.UnitTests.Database
+{
+    [TestFixture]
+    public class DatabaseContextDapper
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase(" ")]
+        [TestCase(" ")]
+        [Test]
+        public void Constructor_WhenPassInvalidConnectionString_ThrowsArgumentNullException(string cns)
+        {
+            Assert.That(() => {
+                new Logic.Database.DatabaseContextDapper(cns);
+            }, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Constructor_WhenPassValidConnectionString_ReturnObject()
+        {
+            var rst = new Logic.Database.DatabaseContextDapper("connectionString");
+            Assert.That(rst, Is.Not.Null);
+            Assert.That(rst, Is.TypeOf<Logic.Database.DatabaseContextDapper>());
+        }
+
+    }
+}
