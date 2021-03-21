586f42e600ddacdd82ac729e4b76dbae59012e94
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-21 13:25:28 +0330

    Add JWT settings to the appsettings.json and Login method to the Teacher.cs

--- SnappetChallenge/Snappet.Logic/Security/Teacher.cs
+++ SnappetChallenge/Snappet.Logic/Security/Teacher.cs
@@ -1,6 +1,31 @@
 ﻿namespace Snappet.Logic.Security
 {
     public class Teacher : Models.Database.StoredProcedures.dbo.SP_Teacher_Login.Inputs
-    {        
+    {
+        /// <summary>
+        /// Validation username & password and generate JWT
+        /// </summary>
+        /// <param name="ctx">Database ORM</param>
+        /// <param name="mapper">The object mapper</param>
+        /// <param name="key">JWT key</param>
+        /// <param name="issuer">JWT issuer</param>
+        /// <returns></returns>
+        public Models.Database.DBResult Login(
+            Database.IDatabaseContext ctx,
+            AutoMapper.IMapper mapper,
+            string key,
+            string issuer)
+        {
+            //Call DB
+            var dbResult = ctx.SP_Teacher_Login(this);
+            if (dbResult.StatusCode != 200)
+                return dbResult;
+
+            //Convert Teacher to User object
+            var user = mapper.Map<User>(dbResult.Data);
+
+            //Generate JWT and return as the result
+            return user.GetJWT(key, issuer);
+        }
     }
 }
