fa57e0330a2fef764aec4256d38636c40e8031f8
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-21 12:37:01 +0330

    Create DBResult.cs

---
+++ SnappetChallenge/Snappet.Models/Database/DBResult.cs
@@ -0,0 +1,38 @@
+﻿namespace Snappet.Models.Database
+{
+    /// <summary>
+    /// Database query result
+    /// </summary>
+    public class DBResult
+    {
+        /// <summary>
+        /// The query result. It could be a list or just a simple Id
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        /// All record count by the current condition
+        /// </summary>
+        public int ActualSize { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public DBResult()
+        {
+        }
+
+        public DBResult(int statusCode, string errorMessage, object data = null, int actualSize = 0)
+        {
+            this.StatusCode = statusCode;
+            this.ErrorMessage = errorMessage;
+
+            this.Data = data;
+            if (actualSize >= 0)
+                this.ActualSize = actualSize;
+            else
+                this.ActualSize = data == null ? 0 : 1;
+        }
+    }
+}
