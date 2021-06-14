14f2641646e1893953bf35ceb26cca6410a2de01
Herman Fransen <hermsky@gmail.com>
2021-06-14 23:26:20 +0200

    added unit test not working yet

--- src/app/shared/services/data.service.spec.ts
+++ src/app/shared/services/data.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { DataService } from './data.service';
+import { mock } from '../test-mock/work-test';
 
 describe('DataService', () => {
   let service: DataService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule 
+        HttpClientTestingModule
+      ],
+      providers: [
+        HttpTestingController
       ]
     })
     .compileComponents();
@@ -18,9 +23,25 @@ describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
+
+  // afterEach(() => {
+  //   httpTestingController.verify();
+  // });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  xit('should give correct "getWork" method', () => {
+    service.getWork('', '', '', '').subscribe((result) => {
+      expect(result).toEqual(mock.work);
+      expect(result.length).toBe(mock.work.length);
+    })
+    let req = httpTestingController.expectOne(`http://localhost:3000/work`);    
+    expect(req.request.method).toEqual('GET');
+    req.flush(mock.work);    
+  });
+
 });
--- src/app/shared/test-mock/work-test.ts
+++ src/app/shared/test-mock/work-test.ts
@@ -1,4 +1,4 @@
-{
+export var mock = {
   "subjects": [
     {
       "id": 1,
