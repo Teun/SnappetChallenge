6a58ac505d04bbf0abdc108933f71e42fe398fe9
Mishan Groenewald <mishangroenewald@gmail.com>
2021-07-26 00:33:50 +0200

    feat(app-routing.module.ts): adding routing for 'student-reporting' library.

--- SnappetChallenge/src/app/app-routing.module.ts
+++ SnappetChallenge/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { StudentReportingComponent } from 'student-reporting';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/student-reporting', pathMatch: 'full' },
+  { path: 'student-reporting', component: StudentReportingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
