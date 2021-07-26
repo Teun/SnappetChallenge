41415709ce855ed1df03f8716ab218d7756ec2de
Mishan Groenewald <mishangroenewald@gmail.com>
2021-07-26 23:29:48 +0200

    revert(student-reporting-library): removing unwanted library and its dependencies.

--- SnappetChallenge/projects/student-reporting/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of student-reporting
- */
-
-export * from './lib/student-reporting.service';
-export * from './lib/student-reporting.component';
-export * from './lib/student-reporting.module';
--- SnappetChallenge/src/app/app-routing.module.ts
+++ SnappetChallenge/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { StudentReportingComponent } from 'student-reporting';
-
 const routes: Routes = [
-  { path: '', redirectTo: '/student-reporting', pathMatch: 'full' },
-  { path: 'student-reporting', component: StudentReportingComponent },
+
 ];
 
 @NgModule({
