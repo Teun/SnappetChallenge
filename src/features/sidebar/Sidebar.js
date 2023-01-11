6301f1cb26e2ac49a37d55f122a90140389cd63c
Sampada Karanjit <sampada.karanjit@gmail.com>
2023-01-11 13:29:24 +0100

    Add sidebar components

---
+++ src/features/sidebar/Sidebar.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons/lib";
+import { SidebarData } from "./SidebarData";
+import SubMenu from "./SubMenu";
+import {
+  Nav,
+  NavIcon,
+  SidebarNav,
+  SidebarWrap,
+} from "./styled_components/styled_sidebar_components";
+
+const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(true);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavIcon to="#">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </NavIcon>
+        </Nav>
+        <SidebarNav sidebar={sidebar} bg="light" variant="light">
+          <SidebarWrap>
+            <NavIcon to="#">
+              <AiIcons.AiOutlineClose onClick={showSidebar} />
+            </NavIcon>
+            {SidebarData.map((item, index) => {
+              return <SubMenu item={item} key={index} />;
+            })}
+          </SidebarWrap>
+        </SidebarNav>
+      </IconContext.Provider>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/features/sidebar/SidebarData.js
@@ -0,0 +1,21 @@
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
+
+export const SidebarData = [
+  {
+    title: "Overview",
+    path: "/dailyoverview",
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+  },
+  {
+    title: "Performance Report",
+    path: "/performancereport",
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+  },
+];
