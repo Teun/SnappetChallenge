330c4f8c272b0c4782c943c77848783c65521f4d
Sampada Karanjit <sampada.karanjit@gmail.com>
2023-01-11 13:30:17 +0100

    add header and home screen components

---
+++ src/features/screens/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FilterSection } from "../filter/store/FilterSection";
+
+export const Header = () => {
+  return (
+    <>
+      <FilterSection />
+    </>
+  );
+};
---
+++ src/features/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DailyOverview } from "../work/components/DailyOverview";
+
+const HomeScreen = ({ match }) => {
+  return (
+    <>
+      <DailyOverview />
+    </>
+  );
+};
+
+export default HomeScreen;
