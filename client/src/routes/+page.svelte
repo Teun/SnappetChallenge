2d6cd298f48b9aa370bf29d894d23c9978d5e317
linjoe <ik@lindseyschaap.nl>
2023-06-18 23:05:14 +0200

    made a start on the server

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+ const client = new ApolloClient({
+  uri: 'localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
+
+client
+  .query({
+    query: gql`
+            query WorkOfTheDay($submitDateTime: String!) {
+                workOfTheDay(SubmitDateTime: $submitDateTime) {
+                    SubmittedAnswers {
+                    SubmitDateTime
+                    Subject
+                    Progress
+                    Domain
+                    LearningObjective
+                    }n
+                    UserId
+                }
+        }
+    `,
+  })
+  .then((result) => console.log(result));
+
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
